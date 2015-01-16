7eb08241de14f87d500f1115a6272ad33aa0864a
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-01-16 13:45:16 +0100

    Added check for git repo

--- pas2/2015-01-16/pas2_tools.ps1
+++ pas2/2015-01-16/pas2_tools.ps1
@@ -6,6 +6,9 @@ if ($cmd -eq 'init') {
 	if (test-path 'PAS2-Hoved') {
 		Write-Error 'PAS2-Hoved already exists'
 	}
+	if (test-path .\.git) {
+		Write-Error 'Current dir is in a Git repo already'
+	}
 	git clone git@github.com:Utdanningsdirektoratet/PAS2-hoved.git
 	scoop update .\PAS2-Hoved\pas2_tools.json
 }
