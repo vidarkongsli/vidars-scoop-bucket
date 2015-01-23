a77108eb520df52d1f3c14f7f9f74e30bcebd60a
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-01-23 15:41:16 +0100

    Fixed replacement

--- firefox/installer.ps1
+++ firefox/installer.ps1
@@ -1,6 +1,6 @@
+
 $iniContents = gc $PSScriptRoot\template.ini
-$iniContents.Replace('$installdir$', "$PSScriptRoot")
-$iniContents > $PSScriptRoot\template.ini
 
+$iniContents.Replace('$installdir$', $PSScriptRoot) > $PSScriptRoot\template.ini
 
-& $PSScriptRoot\Firefox%20Setup%20Stub%2034.0.5.exe /INI="$PSScriptRoot\template.ini"
+#& $PSScriptRoot\Firefox%20Setup%20Stub%2034.0.5.exe /INI="$PSScriptRoot\template.ini"
