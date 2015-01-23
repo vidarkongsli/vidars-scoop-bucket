24b71a9d3c3d29c57bfc68c7a1cb0db830153890
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-01-23 16:21:55 +0100

    Fixed nstall script

--- firefox/installer.ps1
+++ firefox/installer.ps1
@@ -1,6 +1,3 @@
-
-$iniContents = gc $PSScriptRoot\template.ini
-
-$iniContents.Replace('$installdir$', $PSScriptRoot) > $PSScriptRoot\template.ini
+(gc $psScriptRoot\template.ini) -replace '\$installDir\$',$PsScriptRoot > $psScriptRoot\install.ini
 
 #& $PSScriptRoot\Firefox%20Setup%20Stub%2034.0.5.exe /INI="$PSScriptRoot\template.ini"
