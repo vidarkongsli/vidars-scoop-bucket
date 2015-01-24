717f3a7b9590d5563208370f1cf74eae28f5124d
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-01-24 07:00:05 +0100

    Add uninstaller. Fix installer

--- firefox/installer.ps1
+++ firefox/installer.ps1
@@ -1,3 +1,4 @@
 (gc $psScriptRoot\template.ini) -replace '\$installDir\$',$PsScriptRoot > $psScriptRoot\install.ini
+$cmdLine = "`"Firefox%20Setup%2035.0.exe /INI=`"$PSScriptRoot\install.ini`"`""
 
-& $PSScriptRoot\Firefox%20Setup%2035.0.exe /INI="`"$PSScriptRoot\install.ini`"" -ms
+& cmd /C $cmdline
