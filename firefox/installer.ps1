94c86016b87018252064f7e4f05633bf36856329
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-01-24 07:07:58 +0100

    Fix cmd exe

--- firefox/installer.ps1
+++ firefox/installer.ps1
@@ -1,4 +1,4 @@
 (gc $psScriptRoot\template.ini) -replace '\$installDir\$',$PsScriptRoot > $psScriptRoot\install.ini
-$cmdLine = "`"Firefox%20Setup%2035.0.exe /INI=`"$PSScriptRoot\install.ini`"`""
+$cmdLine = "`"`"$PSScriptRoot\Firefox%20Setup%2035.0.exe`" /INI=`"$PSScriptRoot\install.ini`"`""
 
 & cmd /C $cmdline
