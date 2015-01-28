3172938e943f6e82596170d817e05841397ad649
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-01-28 17:01:35 +0100

    Remove uninstaller. ps1 not supported

--- msbuild/uninstaller.ps1
+++
@@ -1,7 +0,0 @@
-$text = gc $profile
-if(($text | sls 'setenvforvs.ps1') -eq $null) {
-    Exit 0
-}
-
-$new_profile = $text -replace [regex]::escape("try { & 'setenvforvs.ps1'} catch { }"), ''
-$new_profile > $profile
