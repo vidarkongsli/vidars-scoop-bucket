453d00c7d2bcff35892bd0309d8296d18b4e7f7b
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-01-28 14:59:47 +0100

    Add Visual Studio scripts

---
+++ msbuild/installer.ps1
@@ -0,0 +1,18 @@
+if(!(test-path $profile)) {
+    $profile_dir = split-path $profile
+    if(!(test-path $profile_dir)) { mkdir $profile_dir > $null } 
+    '' > $profile
+}
+
+$setEnvCommand = "$PSScriptRoot\setenvforvs.ps1"
+
+$text = gc $profile
+if(($text | sls 'setenvforvs') -eq $null) {
+    write-host 'Adding Visual Studio environment init to your powershell profile'
+
+    # read and write whole profile to avoid problems with line endings and encodings
+    $new_profile = @($text) + "try { & '$setEnvCommand'} catch { }"
+    $new_profile > $profile
+} else {
+    write-host 'it looks like the Visual Studio init is already in your powershell profile, skipping'
+}
