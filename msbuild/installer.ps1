cf6dcb5ef8d5201b30af2150d180fadabce7c6b8
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-05-22 09:57:35 +0200

    New install/uninstall

--- msbuild/installer.ps1
+++ msbuild/installer.ps1
@@ -3,14 +3,7 @@ if(!(test-path $profile)) {
     if(!(test-path $profile_dir)) { mkdir $profile_dir > $null } 
     '' > $profile
 }
-
-$text = gc $profile
-if(($text | sls 'setenvforvs') -eq $null) {
-    write-host 'Adding Visual Studio environment init to your powershell profile'
-
-    # read and write whole profile to avoid problems with line endings and encodings
-    $new_profile = @($text) + "try { & 'setenvforvs.ps1'} catch { }"
-    $new_profile > $profile
-} else {
-    write-host 'it looks like the Visual Studio init is already in your powershell profile, skipping'
-}
+$text = (gc $profile) -notmatch 'setenvforvs.ps1' 
+$new_text = @($text) + "try { & 'setenvforvs.ps1'} catch { }"
+$new_text > $profile
+Write-host 'Adding Visual Studio environment init to your powershell profile'
