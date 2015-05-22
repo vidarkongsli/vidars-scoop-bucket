a39fe99393d8836066b6d78984de2d69ea19d73e
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-05-22 09:07:56 +0200

    Update MSbuild to work for VS 2013

--- msbuild/setenvforvs.ps1
+++ msbuild/setenvforvs.ps1
@@ -3,7 +3,8 @@ function _xpd($s) {$ExecutionContext.InvokeCommand.ExpandString($s)}
 get-command msbuild -ErrorAction SilentlyContinue 2>&1 | out-null
 if (-not($?)) {
 
-  $Command = '"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"', `
+  $Command = '"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"', `
+           '"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"', `
            '"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"' `
            | ?{ test-path (_xpd($_).Replace('"','')) -PathType Leaf } `
            | select -First 1
