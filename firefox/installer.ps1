65d51636f50fa3c9865cc9e9e8e92c6b75fa8f41
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-10-23 12:49:46 +0200

    Update Firefox installer for Win 10

--- firefox/installer.ps1
+++ firefox/installer.ps1
@@ -1,4 +1,6 @@
 (gc $psScriptRoot\template.ini) -replace '\$installDir\$',$PsScriptRoot > $psScriptRoot\install.ini
-$cmdLine = "`"`"$PSScriptRoot\Firefox%20Setup%2035.0.exe`" /INI=`"$PSScriptRoot\install.ini`"`""
 
-& cmd /C $cmdline
+$exePath = join-path $PSScriptRoot 'Firefox%20Setup%2035.0.exe'
+$iniPath = join-path $PsScriptRoot 'install.ini'
+$args = "/INI=`"$iniPath`""
+Start-process -FilePath $exePath -ArgumentList $args -Wait
