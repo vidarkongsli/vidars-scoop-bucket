b46855351d103b83e2dca41e1965604b2f4dc740
Vidar Kongsli <vidarkongsli@yahoo.com>
2015-10-23 12:58:06 +0200

    Update ff installer for version 41

--- firefox/installer.ps1
+++ firefox/installer.ps1
@@ -1,6 +1,6 @@
 (gc $psScriptRoot\template.ini) -replace '\$installDir\$',$PsScriptRoot > $psScriptRoot\install.ini
 
-$exePath = join-path $PSScriptRoot 'Firefox%20Setup%2035.0.exe'
+$exePath = join-path $PSScriptRoot 'Firefox%20Setup%2041.0.2.exe'
 $iniPath = join-path $PsScriptRoot 'install.ini'
 $args = "/INI=`"$iniPath`""
 Start-process -FilePath $exePath -ArgumentList $args -Wait
