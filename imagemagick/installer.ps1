5d6d3812561ad0d9f24aa0c9ddaecb2dfa2c0b59
Vidar Kongsli <vidar@kongs.li>
2017-03-21 10:30:20 +0100

    Remove imagemagick

--- imagemagick/installer.ps1
+++
@@ -1,3 +0,0 @@
-(gc $psScriptRoot\imagemagick.inf) -replace '\$installpath\$',$PsScriptRoot > $psScriptRoot\imagemagick000.inf
-$cmdLine = "`"`"$PSScriptRoot\\ImageMagick-6.9.1-1-Q16-x64-dll.exe`" /LOADINF=`"$PSScriptRoot\imagemagick000.inf`"`" /silent" 
-& cmd /C $cmdline
